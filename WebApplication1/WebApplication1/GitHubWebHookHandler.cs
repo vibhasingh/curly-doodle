4feea56476864f1a01fa87c4f0787d8feaca39db
Vibha Singh <v.singh@prowareness.nl>
2018-07-18 13:38:19 +0530

    webhook code git

---
+++ WebApplication1/WebApplication1/GitHubWebHookHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class GitHubWebHookHandler : WebHookHandler
+    {
+        public GitHubWebHookHandler()
+        {
+            this.Receiver = GitHubWebHookReceiver.ReceiverName;
+        }
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+            string action = context.Actions.First();
+            JObject data = context.GetDataOrDefault<JObject>();
+
+            return Task.FromResult(true);
+        }
+
+
+    }
+}
+
+
