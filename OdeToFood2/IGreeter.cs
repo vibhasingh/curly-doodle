df35ce34e0f51f4ad8a2a51fd5266fd21fdd0ef7
Vibha Singh <v.singh@prowareness.nl>
2018-07-19 17:47:20 +0530

    Asp.net core

---
+++ OdeToFood2/IGreeter.cs
@@ -0,0 +1,15 @@
+﻿namespace OdeToFood2
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        public string GetMessageOfTheDay()
+        {
+            return "Greetings";
+        }
+    }
+}
